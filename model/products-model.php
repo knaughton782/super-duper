cb4ec5a9890cea24c919bcaf8500278ca1343a50
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-03-07 23:19:50 -0700

    working update to db, creating delete from db functionality

--- model/products-model.php
+++ model/products-model.php
@@ -19,7 +19,7 @@ function addCategory($categoryName) {
 }
 
 //product function
-function addProduct($invName, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invSize, $invWeight, $invLocation, $invVendor, $invStyle) {
+function addProduct($categoryId, $invName, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invSize, $invWeight, $invLocation, $invVendor, $invStyle) {
 
     $db = acmeConnect();  //create db connection object
     $sql = 'insert into inventory (invName, invDescription, invImage, invThumbnail, invPrice, invStock, invSize, invWeight, invLocation, categoryId,  invVendor, invStyle) values ( :invName, :invDescription, :invImage, :invThumbnail, :invPrice, :invStock, :invSize, :invWeight, :invLocation, :categoryId, :invVendor, :invStyle)'; //sql query
@@ -65,4 +65,37 @@ function getProductInfo($invId){
     $prodInfo = $stmt->fetch(PDO::FETCH_ASSOC);
     $stmt->closeCursor();
     return $prodInfo;
+}
+
+// Update a product
+function updateProduct($invId, $categoryId, $invName, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invSize, $invWeight, $invLocation, $invVendor, $invStyle) {
+        // Create a connection
+        $db = acmeConnect();
+        
+        // The SQL statement to be used with the database
+        $sql = 'UPDATE inventory SET invName = :invName, invDescription = :invDescription, invImage = :invImage, invThumbnail = :invThumbnail, invPrice = :invPrice, invStock = :invStock, invSize = :invSize, invWeight = :invWeight, invLocation = :invLocation, categoryId = :categoryId, invVendor = :invVendor, invStyle = :invStyle WHERE invId = :invId';
+        
+        $stmt = $db->prepare($sql);
+        
+        $stmt->bindValue(':invId', $invId, PDO::PARAM_INT);
+        $stmt->bindValue(':categoryId', $categoryId, PDO::PARAM_INT);
+        $stmt->bindValue(':invName', $invName, PDO::PARAM_STR);
+        $stmt->bindValue(':invDescription', $invDescription, PDO::PARAM_STR);
+        $stmt->bindValue(':invImage', $invImage, PDO::PARAM_STR);
+        $stmt->bindValue(':invThumbnail', $invThumbnail, PDO::PARAM_STR);
+        $stmt->bindValue(':invPrice', $invPrice, PDO::PARAM_STR);
+        $stmt->bindValue(':invStock', $invStock, PDO::PARAM_INT);
+        $stmt->bindValue(':invSize', $invSize, PDO::PARAM_INT);
+        $stmt->bindValue(':invWeight', $invWeight, PDO::PARAM_INT);
+        $stmt->bindValue(':invLocation', $invLocation, PDO::PARAM_STR);
+        $stmt->bindValue(':invVendor', $invVendor, PDO::PARAM_STR);
+        $stmt->bindValue(':invStyle', $invStyle, PDO::PARAM_STR);
+       
+        
+        
+        $stmt->execute();
+        $rowsChanged = $stmt->rowCount();
+        $stmt->closeCursor();
+        
+        return $rowsChanged;
 }
--- products/index.php
+++ products/index.php
@@ -52,22 +52,22 @@ switch ($action) {
         break;
 
     case 'newProd': //delivers add product page
-        
+
         $categoryId = filter_input(INPUT_POST, 'categoryId', FILTER_SANITIZE_NUMBER_INT);
         $invName = filter_input(INPUT_POST, 'invName', FILTER_SANITIZE_STRING);
         $invDescription = filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING);
         $invImage = filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING);
         $invThumbnail = filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING);
         $invPrice = filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_INT, FILTER_FLAG_ALLOW_FRACTION);
-        $invStock = filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_INT); 
+        $invStock = filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_INT);
         $invSize = filter_input(INPUT_POST, 'invSize', FILTER_SANITIZE_NUMBER_INT);
         $invWeight = filter_input(INPUT_POST, 'invWeight', FILTER_SANITIZE_NUMBER_INT);
         $invLocation = filter_input(INPUT_POST, 'invLocation', FILTER_SANITIZE_STRING);
         $invVendor = filter_input(INPUT_POST, 'invVendor', FILTER_SANITIZE_STRING);
         $invStyle = filter_input(INPUT_POST, 'invStyle', FILTER_SANITIZE_STRING);
 
-        if (empty($categoryId) || empty($invName) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invSize) || empty($invWeight) || empty($invLocation) ||  empty($invVendor) || empty($invStyle)) {
-             $_SESSION['message'] = '<p>All form fields are required. Please provide complete information for all form fields.</p>';
+        if (empty($categoryId) || empty($invName) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invSize) || empty($invWeight) || empty($invLocation) || empty($invVendor) || empty($invStyle)) {
+            $_SESSION['message'] = '<p>All form fields are required. Please provide complete information for all form fields.</p>';
             include '../view/add-product.php';
             exit;
         }
@@ -78,11 +78,11 @@ switch ($action) {
 
         // is the return value = 1? One row changed in the db
         if ($prodOutcome === 1) {
-             $_SESSION['message'] = "<p>Thank you for adding $invName! </p>";
+            $_SESSION['message'] = "<p>Thank you for adding $invName! </p>";
             include '../view/add-product.php';
             exit;
         } else {
-             $_SESSION['message'] = '<p class="warning">Sorry! $invName was not added. Please try again.</p>';
+            $_SESSION['message'] = '<p class="warning">Sorry! $invName was not added. Please try again.</p>';
             include '../view/add-product.php';
             exit;
         }
@@ -96,38 +96,75 @@ switch ($action) {
         $invId = filter_input(INPUT_GET, 'invId', FILTER_VALIDATE_INT);
         $prodInfo = getProductInfo($invId);
         if (count($prodInfo) < 1) {
-                $_SESSION['message'] = 'Sorry, no product information could be found.';
-                header('Location: /acme/products/');
-                exit;
+            $_SESSION['message'] = 'Sorry, no product information could be found.';
+            header('Location: /acme/products/');
+            exit;
         }
         include '../view/prod-update.php';
         exit;
         break;
-    
-    
-    
-    
-    default:
-        
-        $products = getProductBasics();
-        if(count($products) > 0){
-                $prodList = '<table>';
-                $prodList .= '<thead>';
-                $prodList .= '<tr><th>Product Name</th><th>&nbsp;</th><th>&nbsp;</th></tr>';
-                $prodList .= '</thead>';
-                $prodList .= '<tbody>';
+
+    case 'updateProd':
+        $invId = filter_input(INPUT_POST, 'invId', FILTER_SANITIZE_NUMBER_INT);
+        $categoryId = filter_input(INPUT_POST, 'categoryId', FILTER_SANITIZE_NUMBER_INT);
+        $invName = filter_input(INPUT_POST, 'invName', FILTER_SANITIZE_STRING);
+        $invDescription = filter_input(INPUT_POST, 'invDescription', FILTER_SANITIZE_STRING);
+        $invImage = filter_input(INPUT_POST, 'invImage', FILTER_SANITIZE_STRING);
+        $invThumbnail = filter_input(INPUT_POST, 'invThumbnail', FILTER_SANITIZE_STRING);
+        $invPrice = filter_input(INPUT_POST, 'invPrice', FILTER_SANITIZE_NUMBER_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $invStock = filter_input(INPUT_POST, 'invStock', FILTER_SANITIZE_NUMBER_INT);
+        $invSize = filter_input(INPUT_POST, 'invSize', FILTER_SANITIZE_NUMBER_INT);
+        $invWeight = filter_input(INPUT_POST, 'invWeight', FILTER_SANITIZE_NUMBER_INT);
+        $invLocation = filter_input(INPUT_POST, 'invLocation', FILTER_SANITIZE_STRING);
+        $invVendor = filter_input(INPUT_POST, 'invVendor', FILTER_SANITIZE_STRING);
+        $invStyle = filter_input(INPUT_POST, 'invStyle', FILTER_SANITIZE_STRING);
+
+        if (empty($categoryId) || empty($invName) || empty($invDescription) || empty($invImage) || empty($invThumbnail) || empty($invPrice) || empty($invStock) || empty($invSize) || empty($invWeight) || empty($invLocation) || empty($invVendor) || empty($invStyle)) {
+                $_SESSION['message'] = '<p>Please complete all information fields including the category. </p>';
                 
-                    foreach ($products as $product) {
-                        $prodList .= "<tr><td>$product[invName]</td>";
-                        $prodList .= "<td><a href='/acme/products?action=mod&invId=$product[invId]' title='Click to modify'>Modify</a></td>";
-                        $prodList .= "<td><a href='/acme/products?action=del&invId=$product[invId]' title='Click to delete'>Delete</a></td></tr>";
-                       }
-                $prodList .= '</tbody></table>';
-                   } else {
-                    $_SESSION['message'] = '<p class="notify">Sorry, no products were returned.</p>';
-                }
+                include '../view/prod-update.php';
+                exit;
+        }
         
+        $updateResult = updateProduct($invId, $categoryId, $invName, $invDescription, $invImage, $invThumbnail, $invPrice, $invStock, $invSize, $invWeight, $invLocation, $invVendor, $invStyle);
         
+        if ($updateResult) {
+                $message = "<p>Congratulations, $invName was successfully updated.</p>";
+                $_SESSION['message'] = $message;
+                header('location: /acme/products/');
+                exit;
+        } 
+        
+        else {
+                $_SESSION['message'] = "<p>Error. The product was not updated.</p>";
+                include '../view/prod-update.php';
+                exit;
+        }
+        
+        break;
+
+
+    default:
+
+        $products = getProductBasics();
+        if (count($products) > 0) {
+            $prodList = '<table>';
+            $prodList .= '<thead>';
+            $prodList .= '<tr><th>Product Name</th><th>&nbsp;</th><th>&nbsp;</th></tr>';
+            $prodList .= '</thead>';
+            $prodList .= '<tbody>';
+
+            foreach ($products as $product) {
+                $prodList .= "<tr><td>$product[invName]</td>";
+                $prodList .= "<td><a href='/acme/products?action=mod&invId=$product[invId]' title='Click to modify'>Modify</a></td>";
+                $prodList .= "<td><a href='/acme/products?action=del&invId=$product[invId]' title='Click to delete'>Delete</a></td></tr>";
+            }
+            $prodList .= '</tbody></table>';
+        } else {
+            $_SESSION['message'] = '<p class="notify">Sorry, no products were returned.</p>';
+        }
+
+
         include '../view/prod-mgmt.php';
         break;
 }
