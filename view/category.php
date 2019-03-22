e93f9dc18d1ce88832092ac40d661414eeaa95f7
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-03-21 19:35:48 -0700

    activities update, products folder update

---
+++ view/category.php
@@ -0,0 +1,24 @@
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/header.php'; ?>
+
+<main id="page-content"> 
+  
+    <h1 class="siteTitle"><?php echo $categoryName; ?> Products</h1>
+    
+    <section>
+        <?php
+        if (isset($_SESSION['message'])) {
+            echo $_SESSION['message'];
+
+            unset ($_SESSION['message']);
+        }
+        ?>
+    
+        <?php if(isset($prodDisplay)){ 
+                echo $prodDisplay; 
+            } 
+        ?>
+    </section>  
+
+</main>
+
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/acme/common/footer.php'; ?>
