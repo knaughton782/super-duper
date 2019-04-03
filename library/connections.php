b124c7c1095bdd6767392c9e06e8e16edd8c38a3
Kirsten Naughton <kirsten.naughton@gmail.com>
2019-04-02 20:07:16 -0700

    updates to iClient for new db, added default pw to connections file

--- library/connections.php
+++ library/connections.php
@@ -1,24 +1,40 @@
 <?php
 
+function acmeConnect() {
+    $server = 'localhost';
+    $dbname = 'acme';
+    $username = 'iClient';
+    $password = 'j6IRAOeUJiDIWXaw';
+    $dsn = "mysql:host=$server;dbname=$dbname";
+    $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
-function acmeConnect(){
- $server = 'localhost';
- $dbname= 'acme';
- $username = 'iClient';
- $password = 'gUjL5swVbXl1EhcV';
- $dsn = "mysql:host=$server;dbname=$dbname";
- $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
-
- // Create the actual connection object and assign it to a variable
- try {
-  $link = new PDO($dsn, $username, $password, $options);
-  return $link;
- } catch(PDOException $e) {
-  header('Location: /acme/view/500.php'); 
-  exit;
- }
+    // Create the actual connection object and assign it to a variable
+    try {
+        $link = new PDO($dsn, $username, $password, $options);
+        return $link;
+    } catch (PDOException $e) {
+        header('Location: /acme/view/500.php');
+        exit;
+    }
 }
 
 acmeConnect();
-
 ?>
+
+
+<!--
+Admin user:
+
+Firstname: Admin
+Lastname: User
+Email: admin@cit336.net
+Password: Sup3rU$er  -->
+
+
+<!--
+non-admin user:
+Firstname: Kirsten
+Lastname: Naughton
+Email: kirsten.naughton@gmail.com
+Password: Password1!
+-->
